2ab859f740b98660d47a51b02b68d5e02054dd5f
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-12-04 16:43:29 +0800

    feat: server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,10 @@ const connectToDb = require('./config/DBConfig');
 const app = express();
 
 // dependencies
-// const authRoutes = require('./api/routes/auth.route');
+const authRoutes = require('./api/routes/auth.route');
+const adopterRoutes = require('./api/routes/adopter.route');
+const petRoutes = require('./api/routes/pet.route');
+const applicationRoutes = require('./api/routes/application.route');
 
 // connect to database
 connectToDb();
@@ -18,6 +21,10 @@ app.use(cors());
 app.use(express.json());
 
 // routes
+app.use('/api/auth', authRoutes);
+app.use('/api/adopter', adopterRoutes);
+app.use('/api/pet', petRoutes);
+app.use('/api/application', applicationRoutes);
 
 // start server
 app.listen(process.env.PORT, () => {
